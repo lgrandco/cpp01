6a66e34a7d74c4e672718c07286ce1032b5903b6
lgrandco <l.grandcoinlaf@gmail.com>
2024-05-09 13:38:07 +0200

    protects on attack

--- ex01/Zombie.cpp
+++ ex01/Zombie.cpp
@@ -4,6 +4,5 @@ int Zombie::count = 0;
 
 void Zombie::set_name(std::string name) { this->name = name; }
 Zombie::Zombie() {}
-// Zombie::Zombie(std::string name) : name(name) { this->count++; }
 Zombie::~Zombie() { std::cout << name << " died" << std::endl; }
 void Zombie::announce() { std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl; }
--- ex03/HumanB.cpp
+++ ex03/HumanB.cpp
@@ -5,7 +5,9 @@ HumanB::HumanB(std::string name) { this->name = name; }
 void HumanB::setWeapon(Weapon& weapon) { this->weapon = &weapon; }
 
 void HumanB::attack() {
-    std::cout << this->name << " attacks with their " << this->weapon->getType() << std::endl;
+    if (this->weapon == NULL)
+        std::cout << this->name << " cannot attack wihout weapon" << std::endl;
+    else std::cout << this->name << " attacks with their " << this->weapon->getType() << std::endl;
 }
 
 HumanB::~HumanB() { std::cout << this->name << " died" << std::endl; }
